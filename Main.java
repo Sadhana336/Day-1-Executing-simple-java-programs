49d628f6ae2306d2a9045304a272bfec5ccf08ef
P.D.Sadhana <pdsadhana8@gmail.com>
2026-06-16 21:40:54 +0530

    added

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
-import java.util.*;
+
+import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int n2=n;
         int g1=0;
-        int g2=0;
-        int g3=0;
+        
         for(int i=1;i<4;i++){
         if(n>=7){
            g1=7;
